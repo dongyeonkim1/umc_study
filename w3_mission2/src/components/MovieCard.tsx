d0c70be2a20a87b5f2bf1df6d15995e0432e0ff0
dongyeonkim1 <dongyeon8136@naver.com>
2025-04-03 04:19:04 +0900

    mission2 #1

--- w3_mission2/src/components/MovieCard.tsx
+++ w3_mission2/src/components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Movie } from "../types/movie";
+import { useNavigate } from "react-router-dom";
 
 interface MovieCardProps {
 movie: Movie;
@@ -7,12 +8,13 @@ movie: Movie;
 
 export default function MovieCard({movie}: MovieCardProps) {
     const [isHovered, setIsHovered] = useState(false);
+    const navigate = useNavigate();
 
     return (
-
-
-        <div className='relative rounded-xl shadow-lg overflow-hidden cursor-pointer
-        w-44 transition-transform duration-500 hover:scale-105' 
+        <div 
+            onClick={() => navigate(`/movie/${movie.id}`)}
+            className='relative rounded-xl shadow-lg overflow-hidden cursor-pointer
+            w-44 transition-transform duration-500 hover:scale-105' 
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
---
+++ w3_mission2/src/pages/NotFoundPage.tsx
@@ -0,0 +1,3 @@
+export default function NotFoundPage() {
+    return <div>못찾겠다꾀꼬리</div>;
+}
