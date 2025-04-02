dbb35941cadfa7d697efe804199c0ef8b9cb0f5e
dongyeonkim1 <dongyeon8136@naver.com>
2025-04-03 01:48:04 +0900

    ADD w3_mission2

---
+++ w3_mission2/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import MoviePage from './pages/MoviePage';
+
+function App() {
+  return (
+    <div className='app-container'>
+        <MoviePage />
+    </div>
+  );
+}
+
+export default App;
